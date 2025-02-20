c7b121b54274159927250b766b68b45995f94b04
ALISSON CESAR ALBUQUERQUE NOGUEIRA <alissonnogueira0468@digitalcollege.internal>
2025-02-20 11:53:21 -0300

    fakestore

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            screens: {
+                'xl': '1180px',
+                '2xl':'1456px',
+            },
+            fontFamily: {
+                sans: ['Open Sans', 'sans-serif']
+                },
+        },
+    },
+    plugins: [],
+}
